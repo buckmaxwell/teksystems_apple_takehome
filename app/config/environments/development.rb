287a6f19aeb23ef57fdad6f065e68f8403fa5a58
Max Buck <maxbuckdeveloper@gmail.com>
2025-03-21 14:27:02 -0400

    Appease Rubocop

--- app/config/environments/development.rb
+++ app/config/environments/development.rb
@@ -1,10 +1,9 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-
   # Debug mode disables concatenation and preprocessing of assets. (Multiple files are not combined into a single file and assets are not minified.)
-  config.assets.debug = true  
-  
+  config.assets.debug = true
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Make code changes take effect immediately without server restart.
